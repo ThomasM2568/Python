38547cd4f4a266631e5584cd5aa19696b997f195
Thomas Mirbey <101272724+ThomasM2568@users.noreply.github.com>
2022-10-31 11:54:38 +0100

    Delete module1.py

--- module1.py
+++
@@ -1,6 +0,0 @@
-﻿nom=input("entrez votre nom :")
-print("Bonjour" ,nom)
-anneedenaissance=int(input("entrez votre année de naissance :"))
-annee=int(input("entrez l'année en cours :"))
-age=annee-anneedenaissance
-print("Tu as",age ,"ans et dans 50 ans tu auras", age+50 )
